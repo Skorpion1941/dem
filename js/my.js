42b8214cbb3436b1d65db975bf87d9a8a554e44c
Skorpion1941 <72256951+Skorpion1941@users.noreply.github.com>
2024-05-16 18:05:34 +0700

    Add files via upload

---
+++ js/my.js
@@ -0,0 +1,11 @@
+function selectStatus (select) {
+    if (select.value == 1) {
+        alert('В ожидании')
+    }
+    if (select.value == 2) {
+        alert('В работе')
+    }
+    if (select.value == 3) {
+        alert('Выполнено')
+    }
+}
